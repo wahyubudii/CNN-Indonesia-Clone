ff7eafed5a3012da70069051995b47ac493b1942
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-02-19 18:06:40 +0700

    display done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [news, setNews] = useState({});
 
   useEffect(() => {
-    document.title = "CNN Indonesia Clone | Homepage"
+    document.title = "CNN Indonesia Clone | Homepage";
 
     async function getNews() {
       const request = await fetch(
@@ -35,64 +35,57 @@ export default function Home() {
               <div className="flex">
                 {news.slice(0, 1).map((newsDisplay) => {
                   return (
-                    <div className="w-8/12">
-                      <img
-                        src={newsDisplay.image.large}
-                        className="h-[470px] w-[760px] object-cover"
-                      />
-                    </div>
+                    <>
+                      <div className="W-8/12 block">
+                        <img
+                          src={newsDisplay.image.large}
+                          className="h-[470px] w-[1000px] object-cover"
+                        />
+                        <a href={newsDisplay.link}>
+                          <div className="px-5 py-4 inline-block relative bg-[#fff] -mt-[70px] shadow-gray-300 shadow-md w-[90%] relative left-[5%] inline-block">
+                            <h3 className="text-2xl text-black-900 opacity-70 font-semibold hover:text-[#c00] transition-all">{newsDisplay.title}</h3>
+                            <div className="flex">
+                              <h3 className="pt-2 text-sm text-red-900 font-semibold text-[#c00]">
+                                Nasional -
+                              </h3>
+                              <h3 className="pt-2 text-sm text-black-900 opacity-50">
+                                - 15 Menit yang lalu
+                              </h3>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
+
+                      <div className="w-4/12 block">
+                        {news.slice(2, 6).map((newsItem) => {
+                          return (
+                            <div className="flex pb-2">
+                              <div className="px-4">
+                                <img
+                                  src={newsItem.image.small}
+                                  className="h-[85px] max-w-sm"
+                                />
+                              </div>
+                              <div>
+                                <p className="text-sm text-black-900 items-start opacity-70 font-semibold leading-5">
+                                  <a href={newsItem.link} target="_blank">
+                                    {newsItem.title}
+                                  </a>
+                                </p>
+                                <p className="text-sm text-red-900 font-semibold">
+                                  Teknologi
+                                </p>
+                                <p className="text-sm text-black-900 opacity-50">
+                                  15 menit lalu
+                                </p>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </>
                   );
                 })}
-
-                <div className="w-4/12 block">
-                  {news.slice(0, 4).map((newsItem) => {
-                    return (
-                      <div className="flex pb-6">
-                        <div className="px-4">
-                          <img
-                            src={newsItem.image.small}
-                            className="h-[80px] max-w-sm"
-                          />
-                        </div>
-                        <div>
-                          <p className="text-sm text-black-900 opacity-70 font-semibold">
-                            <a href={newsItem.link} target="_blank">
-                              {newsItem.title}
-                            </a>
-                          </p>
-                          <p className="text-sm text-red-900 font-semibold">
-                            Teknologi
-                          </p>
-                          <p className="text-sm text-black-900 opacity-50">
-                            15 menit lalu
-                          </p>
-                        </div>
-                      </div>
-                    );
-                  })}
-                  {/* {news.map((newsItem) => {
-                    return (
-                      <div className="flex pb-6">
-                        <div className="px-4">
-                          <img
-                            src={newsItem.image.small}
-                            className="h-[80px] max-w-sm"
-                          />
-                        </div>
-                        <div>
-                          <p className="text-sm text-black-900 opacity-70 font-semibold"><a href={newsItem.link} target="_blank">{newsItem.title}</a>
-                          </p>
-                          <p className="text-sm text-red-900 font-semibold">
-                            Teknologi
-                          </p>
-                          <p className="text-sm text-black-900 opacity-50">
-                            15 menit lalu
-                          </p>
-                        </div>
-                      </div>
-                    );
-                  })} */}
-                </div>
               </div>
             )}
           </div>
